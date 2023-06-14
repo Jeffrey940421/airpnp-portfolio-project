4516dec4662258e957d6ddd0eebe6ee49db8112b
Jeffrey <zhangqy940421@gmail.com>
2023-06-14 09:37:39 -0700

    Fixed typo

--- backend/db/migrations/20230613001205-create-spot-image.js
+++ backend/db/migrations/20230613001205-create-spot-image.js
@@ -34,12 +34,12 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_STAMP")
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_STAMP")
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       }
     }, options);
   },
