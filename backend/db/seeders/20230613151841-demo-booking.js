de2a9b3c348620267874d780aef1763b987451d6
Jeffrey <zhangqy940421@gmail.com>
2024-01-02 15:42:39 -0800

    updated seed data

--- backend/db/seeders/20230613151841-demo-booking.js
+++ backend/db/seeders/20230613151841-demo-booking.js
@@ -63,7 +63,7 @@ module.exports = {
         {spotId: 2, userId: 2, startDate: "2024-02-10", endDate: "2024-02-11"},
         {spotId: 3, userId: 3, startDate: "2024-02-15", endDate: "2024-02-17"},
         {spotId: 3, userId: 1, startDate: "2024-02-17", endDate: "2024-02-20"},
-        {spotId: 1, userId: 1, startDate: "2024-02-20", endDate: "2024-02-30"},
+        {spotId: 1, userId: 1, startDate: "2024-02-20", endDate: "2024-02-25"},
         {spotId: 3, userId: 1, startDate: "2023-12-01", endDate: "2023-12-30"},
         {spotId: 2, userId: 1, startDate: "2023-12-01", endDate: "2023-12-10"}
       ]
