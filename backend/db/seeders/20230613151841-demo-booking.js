052dadc08fb30d188dcd22ce92a0b69625df23f9
Jeffrey <zhangqy940421@gmail.com>
2024-01-02 15:39:13 -0800

    updated seed data

--- backend/db/seeders/20230613151841-demo-booking.js
+++ backend/db/seeders/20230613151841-demo-booking.js
@@ -15,26 +15,26 @@ module.exports = {
       {
         spotId: 2,
         userId: 2,
-        startDate: "2023-12-10",
-        endDate: "2023-12-11"
+        startDate: "2024-02-10",
+        endDate: "2024-02-11"
       },
       {
         spotId: 3,
         userId: 3,
-        startDate: "2023-12-15",
-        endDate: "2023-12-17"
+        startDate: "2024-02-15",
+        endDate: "2024-02-17"
       },
       {
         spotId: 3,
         userId: 1,
-        startDate: "2023-12-17",
-        endDate: "2023-12-20"
+        startDate: "2024-02-17",
+        endDate: "2024-02-20"
       },
       {
         spotId: 1,
         userId: 1,
-        startDate: "2023-12-20",
-        endDate: "2023-12-30"
+        startDate: "2024-02-20",
+        endDate: "2024-02-30"
       }
     ], {validate: true});
 
@@ -43,14 +43,14 @@ module.exports = {
       {
         spotId: 3,
         userId: 1,
-        startDate: "2023-10-01",
-        endDate: "2023-10-30"
+        startDate: "2023-12-01",
+        endDate: "2023-12-30"
       },
       {
         spotId: 2,
         userId: 1,
-        startDate: "2023-10-01",
-        endDate: "2023-10-10"
+        startDate: "2023-12-01",
+        endDate: "2023-12-10"
       }
     ]);
   },
@@ -60,12 +60,12 @@ module.exports = {
     const {Op} = require("sequelize");
     await queryInterface.bulkDelete(options, {
       [Op.or]: [
-        {spotId: 2, userId: 2, startDate: "2023-12-10", endDate: "2023-12-11"},
-        {spotId: 3, userId: 3, startDate: "2023-12-15", endDate: "2023-12-17"},
-        {spotId: 3, userId: 1, startDate: "2023-12-17", endDate: "2023-12-20"},
-        {spotId: 1, userId: 1, startDate: "2023-12-20", endDate: "2023-12-30"},
-        {spotId: 3, userId: 1, startDate: "2023-10-01", endDate: "2023-10-30"},
-        {spotId: 2, userId: 1, startDate: "2023-10-01", endDate: "2023-10-10"}
+        {spotId: 2, userId: 2, startDate: "2024-02-10", endDate: "2024-02-11"},
+        {spotId: 3, userId: 3, startDate: "2024-02-15", endDate: "2024-02-17"},
+        {spotId: 3, userId: 1, startDate: "2024-02-17", endDate: "2024-02-20"},
+        {spotId: 1, userId: 1, startDate: "2024-02-20", endDate: "2024-02-30"},
+        {spotId: 3, userId: 1, startDate: "2023-12-01", endDate: "2023-12-30"},
+        {spotId: 2, userId: 1, startDate: "2023-12-01", endDate: "2023-12-10"}
       ]
     }, {});
   }
